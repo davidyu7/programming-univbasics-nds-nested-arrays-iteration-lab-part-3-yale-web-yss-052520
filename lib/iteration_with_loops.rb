6d7aaa2a8b5fdef63a5b4f15af0a31d448e19a89
David Yu <david.yu@yale.edu>
2020-05-20 16:05:48 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,8 @@
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+  output = ""
   for array in src do
     for i in array do
-      if 
+      if i.class == String
 end
